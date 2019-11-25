dd7798279cd4f40ca2c5dfb6d971fa04ac12049e
Charles Barksdale <charles.barksdale@loop.colum.edu>
2019-11-25 13:35:23 -0600

    Final Game POC

---
+++ FinalGame/FinalGame/Map.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalGame
+{
+    class Map
+    {
+        private int tileWidth;
+        private int tileHeight;
+        private int width;
+        private int height;
+
+        public Map(int ptileWidth, int ptileHeight, int pwidth, int pheight)
+        {
+            tileWidth = ptileWidth;
+            tileHeight = ptileHeight;
+            width = pwidth;
+            height = pheight;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 tilePosition = Vector2.Zero;
+
+            spriteBatch.Begin();
+
+            for (int w = 0; w < width; w++ )
+            {
+                for (int h = 0; h < height; h++)
+                {
+                    spriteBatch.FillRectangle(tilePosition, new Size2(tileWidth, tileHeight), Color.White);
+                    spriteBatch.FillRectangle(tilePosition + new Vector2(2,1), new Size2(tileWidth - 2, tileHeight - 2), Color.Black);
+                    tilePosition.Y += tileHeight;
+                }
+                tilePosition.Y = 0;
+                tilePosition.X += tileWidth;
+            }
+            spriteBatch.End();
+        }
+
+    }
+}
